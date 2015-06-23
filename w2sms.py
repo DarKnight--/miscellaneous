5d33464c40a731bc5bf75cf8adc765cf1b1d30b5
DarKnight <05.DarKnight.11@gmail.com>
2015-06-23 12:23:31 +0530

    Added get_ccontact method in w2sms.py

--- w2sms.py
+++ w2sms.py
@@ -4,7 +4,7 @@ import requests
 import webbrowser
 import warnings
 
-def __warning__(message):
+def _warning(message):
     warnings.warn(category=Warning,
                   message=message)
 
@@ -16,66 +16,101 @@ class W2Sms:
         self.cookies = None
         self.host = "site21.way2sms.com"
         self.headers = {
-                            "Host" : self.host,
-                            "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0",
-                            "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                            "Accept-Language" : "en-US,en;q=0.5",
-                            "Accept-Encoding" : "gzip, deflate",
-                            "Referer" : self.base_url+"content/index.html"
-                        }
-        self.authenticate(filename)
+                            "Host": self.host,
+                            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0",
+                            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                            "Accept-Language": "en-US,en;q=0.5",
+                            "Accept-Encoding": "gzip, deflate",
+                            "Referer": self.base_url+"content/index.html",
+                            "X-Requested-With": None,
+                            "Content-Type": None,
 
+                        }
+        self.uname = None
+        self.pwd = None
+        self.redirect = None
+        self.authenticate(filename)
 
     def authenticate(self, filename):
         with open(filename,"r") as files:
             self.uname = files.readline().strip("\n")
             self.pwd = files.readline().strip("\n")
-        data= {
-                    "username" : self.uname,
-                    "password" : self.pwd
+        _data = {
+                    "username": self.uname,
+                    "password": self.pwd
         }
-        request = requests.post(self.base_url+"Login1.action", data=data, headers=self.headers,
-                                     allow_redirects=False)
+        request = requests.post(self.base_url+"Login1.action", data=_data, headers=self.headers, allow_redirects=False)
         self.redirect = request.headers["location"]
         if "JSESSIONID" in request.cookies:
-            self.cookies = str(request.cookies["JSESSIONID"])
+            self.headers["Cookie"] = "JSESSIONID="+request.cookies["JSESSIONID"]
         else:
-            __warning__("Authentication Failed.Please check your auth file or run forget_password method")
+            _warning("Authentication Failed.Please check your auth file or run forget_password method")
 
+    def __send(self, message, number):
+        self.headers["Referer"] = self.base_url+"sendSMS?Token="+self.headers["Cookie"][15:]
 
-    def __send__(self, message, number):
-        self.headers["Referer"] = self.base_url+"sendSMS?Token="+self.cookies[4:]
-        self.headers["Cookie"] = "JSESSIONID="+self.cookies
-
-        data = {
+        _data = {
             "ssaction" : "ss",
-            "Token" : self.cookies[4:],
+            "Token" : self.headers["Cookie"][15:],
             "mobile" : number,
             "message" : message,
             "msgLen" : str(140-len(message))
         }
-        request = requests.post(self.base_url+"smstoss.action", data=data, headers=self.headers)
+        request = requests.post(self.base_url+"smstoss.action", data=_data, headers=self.headers)
         if str(request.status_code) == '200':
             print "Message sent successfully"
         else:
-            __warning__("Message not sent")
+            _warning("Message not sent")
 
     def send_sms(self, message, number):
-        if self.cookies:
+        if "Cookie" in self.headers:
             length = len(message)
             for index in range(0,length, 140):
-                self.__send__(message[index:index+140], number)
+                self.__send(message[index:index+140], number)
         else:
-            __warning__("Not Authenticated. Please authenticate by running authenticate method")
+            _warning("Not Authenticated. Please authenticate by running authenticate method")
 
     def forgot_password(self):
-        if self.cookies:
+        if "Cookie" in self.headers:
             webbrowser.open_new("http://site21.way2sms.com/wpwd.action")
-        webbrowser.open_new(str(self.location))
+        webbrowser.open_new(str(self.redirect))
+
+    def get_contacts(self):
+        url = self.base_url+"getContacts"
+        self.headers["Referer"] = self.base_url+"main.action?section=s&Token="+self.headers["Cookie"][15:]+"&vfType=register_verify"
+        self.headers["X-Requested-With"] = "XMLHttpRequest"
+        self.headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
+        _data = {"Token" : self.headers["Cookie"][15:]}
+        request = requests.post(url, data=_data, headers=self.headers, allow_redirects=False)
+        print request.headers
+        print request.status_code
+        print request.text
+
+
+    def add_contact(self, name, number, group_id=0):
+        url = self.base_url+"addressbook"
+        self.headers["Referer"] = self.base_url+"main.action?section=s&Token="+self.headers["Cookie"][15:]+"&vfType=register_verify"
+        self.headers["X-Requested-With"] = "XMLHttpRequest"
+        self.headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
+        _data = {
+            "action" : "addcontact",
+            "contno" : number,
+            "contname" : name,
+            "groupid" : group_id,
+            "Token" : self.headers["Cookie"][15:],
+            "ocontno" : "",
+            "ogroupid" : "",
+        }
+        request = requests.post(url, data=_data, headers=self.headers, allow_redirects=False)
+        print request.headers
+        print request.status_code
+        print request.text
 
 
 
 if __name__ == '__main__':
     data = W2Sms(filename="w2sms.auth")
-    data.send_sms("Amishfsf", "9827904271")
+    #data.send_sms("Amishfsf", "9827904271")
+    data.add_contact("test", "3652987456")
+    data.get_contacts()
 
