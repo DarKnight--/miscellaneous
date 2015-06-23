f556ccfa79fe4c5cb9322802fd5a66b51aa73bbe
DarKnight <05.DarKnight.11@gmail.com>
2015-06-23 13:29:07 +0530

    fixed slight changes

--- w2sms.py
+++ w2sms.py
@@ -10,6 +10,7 @@ def _warning(message):
     warnings.warn(category=Warning,
                   message=message)
 
+
 class W2Sms:
 
     def __init__(self, filename):
@@ -17,28 +18,28 @@ class W2Sms:
         self.cookies = None
         self.host = "site21.way2sms.com"
         self.headers = {
-                            "Host": self.host,
-                            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0",
-                            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                            "Accept-Language": "en-US,en;q=0.5",
-                            "Accept-Encoding": "gzip, deflate",
-                            "Referer": self.base_url+"content/index.html",
-                            "X-Requested-With": None,
-                            "Content-Type": None,
+            "Host": self.host,
+            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate",
+            "Referer": self.base_url+"content/index.html",
+            "X-Requested-With": None,
+            "Content-Type": None,
 
-                        }
+        }
         self.uname = None
         self.pwd = None
         self.redirect = None
         self.authenticate(filename)
 
     def authenticate(self, filename):
-        with open(filename,"r") as files:
+        with open(filename, "r") as files:
             self.uname = files.readline().strip("\n")
             self.pwd = files.readline().strip("\n")
         _data = {
-                    "username": self.uname,
-                    "password": self.pwd
+            "username": self.uname,
+            "password": self.pwd
         }
         request = requests.post(self.base_url+"Login1.action", data=_data, headers=self.headers, allow_redirects=False)
         self.redirect = request.headers["location"]
@@ -51,19 +52,18 @@ class W2Sms:
         self.headers["Referer"] = self.base_url+"sendSMS?Token="+self.headers["Cookie"][15:]
 
         _data = {
-            "ssaction" : "ss",
-            "Token" : self.headers["Cookie"][15:]+"z",
-            "mobile" : number,
-            "message" : message,
-            "msgLen" : str(140-len(message))
+            "ssaction": "ss",
+            "Token": self.headers["Cookie"][15:]+"z",
+            "mobile": number,
+            "message": message,
+            "msgLen": str(140-len(message))
         }
         requests.post(self.base_url+"smstoss.action", data=_data, headers=self.headers, allow_redirects=False)
-
 
     def send_sms(self, message, number):
         if "Cookie" in self.headers:
             length = len(message)
-            for index in range(0,length, 140):
+            for index in range(0, length, 140):
                 self.__send(message[index:index+140], number)
         else:
             _warning("Not Authenticated. Please authenticate by running authenticate method")
@@ -74,18 +74,18 @@ class W2Sms:
         webbrowser.open_new(str(self.redirect))
 
     def get_contacts(self):
-        url = self.base_url+"getContacts"
-        self.headers["Referer"] = self.base_url+"main.action?section=s&Token="+self.headers["Cookie"][15:]+"&vfType=register_verify"
+        url = self.base_url + "getContacts"
+        self.headers["Referer"] = self.base_url+"main.action?section=s&Token=" + \
+            self.headers["Cookie"][15:]+"&vfType=register_verify"
         self.headers["X-Requested-With"] = "XMLHttpRequest"
         self.headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
-        _data = {"Token" : self.headers["Cookie"][15:]}
+        _data = {"Token": self.headers["Cookie"][15:]}
         return json.dumps(requests.post(url, data=_data, headers=self.headers, allow_redirects=False).text)
-
-
-
+    
     def add_contact(self, name, number, group_id='0'):
-        url = self.base_url+"addressbook"
-        self.headers["Referer"] = self.base_url+"main.action?section=s&Token="+self.headers["Cookie"][15:]+"&vfType=register_verify"
+        url = self.base_url + "addressbook"
+        self.headers["Referer"] = self.base_url+"main.action?section=s&Token=" + \
+            self.headers["Cookie"][15:]+"&vfType=register_verify"
         self.headers["X-Requested-With"] = "XMLHttpRequest"
         self.headers["Content-Type"] = "application/x-www-form-urlencoded; charset=UTF-8"
         _data = {
@@ -105,7 +105,6 @@ class W2Sms:
 
 if __name__ == '__main__':
     data = W2Sms(filename="w2sms.auth")
-    #data.add_contact("test", "3652987456")
+    # data.add_contact("test", "3652987456")
     data.get_contacts()
-    #data.send_sms("Amishfsf", "9039943712")
-
+    # data.send_sms("Amishfsf", "")
